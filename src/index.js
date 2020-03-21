3fc940d1e0341fa5d977a445384adf4d62df9968
Max <max.khorsun@gmail.com>
2020-03-21 18:34:09 +0300

    feat: fix some problems with brackets

--- src/index.js
+++ src/index.js
@@ -4,75 +4,91 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let array = [];
-    let string = expr;
-    let rezArray = [];
 
-    //make array from string
-    for (let i = 0; i < string.length; i++) {
-        string = string.replace(/ /g, '');
-        if (string[i] == '+' || string[i] == '-' || string[i] == '*' || string[i] == '/') {
-            array.push(string.slice(0, i), string.slice(i, i + 1));
-            string = string.slice(i + 1, string.length);
-            i = 0;
+    //split string to array by tokens
+    let arr = expr.match(/[-+*/()]|\d+/g);
+
+    //test pairs of brackets
+    let countLeftBracket = 0;
+    let countRightBracket = 0;
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] == '(') {
+            countLeftBracket++;
+        } else if (expr[i] == ')') {
+            countRightBracket++;
+        }
+    };
+
+    if (countLeftBracket != countRightBracket) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    };
+
+    //calculator
+    const calculate = (a, b, operator) => {
+        if (operator == '+') { return a + b };
+        if (operator == '-') { return a - b };
+        if (operator == '*') { return a * b };
+        if (operator == '/') {
+            if (b == 0) {
+                throw new Error('TypeError: Division by zero.')
+            } else return a / b
         }
     }
-    array.push(string);
 
-    //make new array
-    rezArray = parseInt(array[0]);
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] == '+') {
-            rezArray += parseInt(array[i + 1]);
-        } else
-            if (array[i] == '-') {
-                rezArray -= parseInt(array[i + 1]);
-            } else
-                if (array[i] == '*') {
-                    rezArray *= parseInt(array[i + 1]);
-                } else
-                    if (array[i] == '/') {
-                        if (array[i + 1] != 0) {
-                            rezArray /= parseInt(array[i + 1]);
-                        } else throw new Error('Devision by zero!');
-                    }
+    let priority = {
+        '(': 1,
+        ')': 1,
+        '+': 2,
+        '-': 2,
+        '*': 3,
+        '/': 3,
+    };
+
+    let answer = 0;
+    let numbers = [];
+    let operators = [];
+
+    const changeStack = () => {
+        answer = calculate(numbers[numbers.length - 2], numbers[numbers.length - 1], operators[operators.length - 1]);
+        numbers.pop();
+        numbers.pop();
+        operators.pop();
+        numbers.push(answer);
     }
-    //console.log(array);
 
-    //VERSION2
-    //   for (let i = 1; i < array.length; i++) {
-    //     if (array[i] == '/') {
-    //       let temp = parseFloat(array[i-1]) / parseFloat(array[i+1]);
-    //       array[i-1] = temp;      
-    //       array.splice(i, i-1);  
-    //     }
-    //     if (array[i] == '*') {
-    //       let temp = parseFloat(array[i-1]) * parseFloat(array[i+1]);
-    //       array[i-1] = temp;      
-    //       array.splice(i, i-1);  
-    //     }
+    if (arr.length == 1) {
+        answer = arr[0]
+    } else {
 
-    //   }  
-    //   console.log(array);
-    //     for (let i = 1; i < array.length; i++) {
-    //     if (array[i] == '+') {
-    //       let temp = parseFloat(array[i-1]) + parseFloat(array[i+1]);
-    //       array[i-1] = temp;      
-    //       array.splice(i, i-1);  
-    //       i = 1;
-    //     }
-    //     if (array[i] == '-') {
-    //       let temp = parseFloat(array[i-1]) - parseFloat(array[i+1]);
-    //       array[i-1] = temp;      
-    //       array.splice(i, i-1);  
-    //       i = 1;
-    //     }
-    //   }   
-    //VERSION2 END 
+        for (let i = 0; i < arr.length; i++) {
+            if (!isNaN(arr[i])) {
+                numbers.push(parseInt(arr[i]));
+            } else if (operators.length == 0 || arr[i] == '(' || priority[arr[i]] > priority[operators[operators.length - 1]]) {
+                operators.push(arr[i]);
+            } else {
+                if (arr[i] == ')') {
+                    if (operators[operators.length - 1] == '(') {
+                        operators.pop();
+                    } else {
+                        changeStack();
+                        i--;
+                    }
+                } else {
+                    changeStack();
+                    i--;
+                }
+            }
+        }
 
-    //console.log(array);
-    //console.log(rezArray);
-    return rezArray;
+        if (numbers.length > 1) {
+            for (let i = 0; i < numbers.length; i++) {
+                changeStack();
+            }
+        }
+
+    }
+
+    return answer;
 }
 
 module.exports = {
